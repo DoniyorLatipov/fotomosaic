d1e1ddf6cfaa91ffb8da5a42a86742f7569b07e7
Doniyor Latipov <doniyor.main.contact@gmail.com>
2024-05-07 22:26:51 +0500

    backup version before mass refactoring

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-function createTable(columns) {
+function createTable(columns, rowRepetition) {
   const table = document.getElementById('table');
+  table.style.gridTemplateColumns = `repeat(${columns * 2}, 1fr)`;
 
   const place = '<div class="pic" data-free="true"></div>';
   const smallPlace = '<div class="pic smallPic" data-free="true"></div>';
@@ -11,15 +12,24 @@ function createTable(columns) {
   `;
 
   table.innerHTML = '';
-  table.insertAdjacentHTML('beforeend', `${currentRows.repeat(6)}`);
+  table.insertAdjacentHTML('beforeend', `${currentRows.repeat(rowRepetition)}`);
 }
 
-createTable(8);
+function changeRowMode() {
+  const rowChangeInput = document.getElementById('rowMode');
+  rowChangeInput.addEventListener('change', (e) => {
+    const [columns, rows] = e.target.value.split(' ').map((el) => parseInt(el));
+    createTable(columns, rows);
+  });
+}
+
+changeRowMode();
+createTable(4, 3);
 
 function adjustTableSize() {
   const table = document.getElementById('table');
   const width = table.clientWidth;
-  const height = width * (3 / 2);
+  const height = width * 1.48;
   table.style.width = width + 'px';
   table.style.height = height + 'px';
 }
@@ -28,14 +38,14 @@ adjustTableSize();
 window.addEventListener('resize', adjustTableSize);
 
 function fillImages() {
-  const btn = document.getElementById('fill');
+  const fillBtn = document.getElementById('fillBtn');
 
-  btn.addEventListener('click', () => {
-    const files = document.getElementById('input').files;
+  fillBtn.addEventListener('click', () => {
+    const files = document.getElementById('inputImages').files;
     const picBlocks = document.querySelectorAll('[data-free="true"]');
 
     for (let i = 0; i < files.length && i < picBlocks.length; i += 1) {
-      let imgLink = URL.createObjectURL(files[i]);
+      const imgLink = URL.createObjectURL(files[i]);
       picBlocks[i].style.backgroundImage = `url(${imgLink})`;
       picBlocks[i].dataset.free = 'false';
     }
@@ -44,56 +54,170 @@ function fillImages() {
 
 fillImages();
 
-function switchMode() {
-  const switchBtn = document.getElementById('switchBtn');
+function renderMode() {
+  const switchBtn = document.getElementById('renderBtn');
   const table = document.getElementById('table');
 
   switchBtn.addEventListener('click', () => {
     if (table.style.width === '1000px') {
-      table.style.width = '90cm';
+      table.removeEventListener('click', select);
+      table.style.cssText = `
+      width: 90cm;
+      column-gap: 0;
+      row-gap: 0;`;
     } else {
-      table.style.width = '1000px';
+      table.addEventListener('click', select);
+      table.style.cssText = `
+      width: 1000px;
+      column-gap: 2px;
+      row-gap: 2px;`;
     }
     adjustTableSize();
   });
 }
 
-switchMode();
+renderMode();
 
-function changeImages() {
-  const picBlocks = document.querySelectorAll('.pic');
-
-  for (const pic of picBlocks) {
-    pic.addEventListener('click', (e) => {
-      e.target.style.outline = '2px solid black';
-      changeMenu(e.target);
-    });
+function select(e) {
+  if (e.target.classList.contains('pic')) {
+    e.target.classList.toggle('selected');
+    if (countSelected() !== 0) {
+      selectModeAvailable(true);
+    } else {
+      selectModeAvailable(false);
+    }
   }
 }
 
-function changeMenu(pic) {
-  const changeMenu = document.querySelector('.change');
-  const changeBtn = document.getElementById('changeBtn');
-  const close = document.getElementById('closeBtn');
+function selectImages() {
+  const table = document.getElementById('table');
 
-  changeMenu.style.display = 'block';
+  table.addEventListener('click', select);
+}
 
-  changeBtn.addEventListener('click', changeImage);
-  function changeImage() {
-    const input = document.getElementById('changeInput');
-    const imgLink = URL.createObjectURL(input.files[0]);
-    pic.style.backgroundImage = `url(${imgLink})`;
-    pic.dataset.free = 'false';
-    changeBtn.removeEventListener('click', changeImage);
-    changeMenu.style.display = 'none';
-    pic.style.outline = 'none';
+function renderCounter() {
+  const value = countSelected();
+  counter.innerHTML = value === 0 ? '' : `Selected: ${value}`;
+}
+
+function countSelected() {
+  const length = getSelected().length;
+  return length;
+}
+
+function getSelected() {
+  return document.getElementsByClassName('selected');
+}
+
+selectImages();
+
+function selectModeAvailable(boolean) {
+  const mainForm = document.getElementById('mainForm');
+  const selectForm = document.getElementById('selectForm');
+  if (boolean) {
+    mainForm.style.display = 'none';
+    selectForm.style.display = 'block';
+  } else {
+    mainForm.style.display = 'block';
+    selectForm.style.display = 'none';
   }
+  renderCounter();
+}
 
-  close.addEventListener('click', () => {
-    changeMenu.style.display = 'none';
-    pic.style.outline = 'none';
-    changeBtn.removeEventListener('click', changeImage);
+function selectModeMenu() {
+  const changeBtn = document.getElementById('changeBtn');
+  const deselectBtn = document.getElementById('deselectBtn');
+  const clearBtn = document.getElementById('clearBtn');
+
+  changeBtn.addEventListener('click', () => {
+    const changedImages = document.getElementById('changeInput').files;
+    const selected = getSelected();
+    if (selected.length > changedImages.length) {
+      if (!confirm('Выделенных мест больше, чем изображений.\nВы хотите продолжить?')) {
+        return;
+      }
+    }
+    for (let i = 0; i < changedImages.length && i < selected.length; i += 1) {
+      const imgLink = URL.createObjectURL(changedImages[i]);
+      selected[i].style.backgroundImage = `url(${imgLink})`;
+      selected[i].dataset.free = 'false';
+    }
+    deselectAll();
+  });
+
+  deselectBtn.addEventListener('click', deselectAll);
+
+  clearBtn.addEventListener('click', () => {
+    const selected = getSelected();
+    if (confirm('Вы уверены что хотите очистить выделенные ячейки?')) {
+      for (let i = 0; i < selected.length; i += 1) {
+        selected[i].style.cssText = `
+          background: gray 50% 50%;
+          background-size: cover;`;
+        selected[i].dataset.free = true;
+      }
+    }
+    deselectAll();
   });
 }
 
-changeImages();
+function deselectAll() {
+  const selected = getSelected();
+  const length = selected.length;
+  for (let i = 0; i < length; i += 1) {
+    selected[0].classList.remove('selected');
+  }
+  countSelected();
+  selectModeAvailable(false);
+}
+
+(function moveSelected() {
+  function createPosition(el) {
+    if (!el.style.backgroundPosition) {
+      el.style.backgroundPosition = '50% 50%';
+    }
+  }
+
+  window.addEventListener('keydown', (e) => {
+    const selected = getSelected();
+    switch (e.code) {
+      case 'KeyW':
+        for (let i = 0; i < selected.length; i += 1) {
+          createPosition(selected[i]);
+          selected[i].style.backgroundPositionY = `${
+            parseInt(selected[i].style.backgroundPositionY) + 10
+          }%`;
+        }
+        break;
+      case 'KeyS':
+        for (let i = 0; i < selected.length; i += 1) {
+          createPosition(selected[i]);
+          selected[i].style.backgroundPositionY = `${
+            parseInt(selected[i].style.backgroundPositionY) - 10
+          }%`;
+        }
+        break;
+      case 'KeyA':
+        for (let i = 0; i < selected.length; i += 1) {
+          createPosition(selected[i]);
+          console.log(selected[i].style.backgroundPositionX);
+          selected[i].style.backgroundPositionX = `${
+            parseInt(selected[i].style.backgroundPositionX) - 10
+          }%`;
+        }
+        break;
+      case 'KeyD':
+        for (let i = 0; i < selected.length; i += 1) {
+          createPosition(selected[i]);
+          selected[i].style.backgroundPositionX = `${
+            parseInt(selected[i].style.backgroundPositionX) + 10
+          }%`;
+        }
+        break;
+      default:
+        break;
+    }
+  });
+})();
+
+selectModeMenu();
